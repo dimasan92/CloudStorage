8e83d29467ba8084e37bf39c21328ee1ce32daf0
dimasan92 <dimasan92@gmail.com>
2018-05-12 15:27:35 +0300

    button's listeners

--- AndroidClient/main/src/main/java/ru/geekbrains/main/authentication/AuthenticationActivity.java
+++ AndroidClient/main/src/main/java/ru/geekbrains/main/authentication/AuthenticationActivity.java
@@ -2,14 +2,37 @@ package ru.geekbrains.main.authentication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import ru.geekbrains.main.R;
 
 public class AuthenticationActivity extends AppCompatActivity {
+
+    // флаг для управления окнами регистрации/авторизации
+    private boolean registration = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
     }
+
+        // обработчк нажатий кнопоки "Регистрация"
+    public void onClickReg(View view) {
+        registration = true;
+        authFormVisible();
+    }
+
+    // обработчк нажатий кнопоки "Авторизация"
+    public void onClickAuth(View view) {
+        registration = false;
+        authFormVisible();
+    }
+
+    // обработчк нажатий кнопоки "Отправить"
+    public void onClickSend(View view) {
+        authentication();
+    }
+
 }
