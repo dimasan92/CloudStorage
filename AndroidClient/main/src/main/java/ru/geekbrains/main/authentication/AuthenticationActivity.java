ddb6d191cfb139fd42757686cd2d338e4a31432f
dimasan92 <dimasan92@gmail.com>
2018-05-12 15:35:53 +0300

    reaction on button's clicks reg/auth

--- AndroidClient/main/src/main/java/ru/geekbrains/main/authentication/AuthenticationActivity.java
+++ AndroidClient/main/src/main/java/ru/geekbrains/main/authentication/AuthenticationActivity.java
@@ -1,8 +1,14 @@
 package ru.geekbrains.main.authentication;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import ru.geekbrains.main.R;
 
@@ -11,14 +17,27 @@ public class AuthenticationActivity extends AppCompatActivity {
     // флаг для управления окнами регистрации/авторизации
     private boolean registration = false;
 
+    // элементы пользовательского интерфейса
+    private ConstraintLayout mainLayout;
+    private LinearLayout authenticationLayout;
+    private TextView tvTitle;
+    private TextView tvNickname;
+    private EditText etNickname;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // находим все элементы интерфейса в xml
         setContentView(R.layout.activity_authentication);
+        mainLayout = findViewById(R.id.main_layout);
+        authenticationLayout = findViewById(R.id.authentication_layout);
+        tvTitle = findViewById(R.id.tv_title);
+        tvNickname = findViewById(R.id.tv_nickname);
+        etNickname = findViewById(R.id.et_nickname);
     }
 
-        // обработчк нажатий кнопоки "Регистрация"
+    // обработчк нажатий кнопоки "Регистрация"
     public void onClickReg(View view) {
         registration = true;
         authFormVisible();
@@ -35,4 +54,23 @@ public class AuthenticationActivity extends AppCompatActivity {
         authentication();
     }
 
+    // показывать поля для регистрации или авторизации, в соответствии с выбором пользователя
+    private void authFormVisible() {
+        ConstraintSet set = new ConstraintSet();
+        set.clone(mainLayout);
+        set.clear(R.id.buttons_layout, ConstraintSet.BOTTOM);
+        TransitionManager.beginDelayedTransition(mainLayout);
+        set.applyTo(mainLayout);
+
+        if (registration) {
+            tvTitle.setText(R.string.registration);
+            tvNickname.setVisibility(View.VISIBLE);
+            etNickname.setVisibility(View.VISIBLE);
+        } else {
+            tvTitle.setText(R.string.authorization);
+            tvNickname.setVisibility(View.GONE);
+            etNickname.setVisibility(View.GONE);
+        }
+        authenticationLayout.setVisibility(View.VISIBLE);
+    }
 }
