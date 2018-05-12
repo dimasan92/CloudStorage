7a44591cbb7db898c304d9cb9aa97ee3ebd6093e
dimasan92 <dimasan92@gmail.com>
2018-05-12 14:26:05 +0300

    Move Activity in package

--- AndroidClient/main/src/main/java/ru/geekbrains/main/authentication/AuthenticationActivity.java
+++ AndroidClient/main/src/main/java/ru/geekbrains/main/authentication/AuthenticationActivity.java
@@ -1,7 +1,9 @@
-package ru.geekbrains.main;
+package ru.geekbrains.main.authentication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ru.geekbrains.main.R;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
