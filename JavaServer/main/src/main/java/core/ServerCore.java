a2cdce3ca217a6749bdf94203a48b02d498b5165
dimasan92 <dimasan92@gmail.com>
2018-05-12 16:36:05 +0300

    server wait fo clients

--- JavaServer/main/src/main/java/core/ServerCore.java
+++ JavaServer/main/src/main/java/core/ServerCore.java
@@ -5,11 +5,24 @@ import common.Constants;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerCore {
 
+    private ServerSocket serverSocket;              // сокет, определяемый при создании сервера
+
+    private List<UserHandler> connectedUsers;       // список подключенных на текущий момент клиентов
+    private ExecutorService usersExecutorService;   // управление нитями для пользователей
+
     public ServerCore() {
-//        connectedUsers = Collections.synchronizedList(new ArrayList<>());
+        connectedUsers = Collections.synchronizedList(new ArrayList<>());
 //        Database dataBase = new CloudStorageDatabase();
 //        dataBase.connect();
 //        storageService = new SimpleFileStorage(dataBase);
@@ -22,11 +35,11 @@ public class ServerCore {
             System.err.println("Ошибка при запуске сервера(порт " + Constants.SERVER_PORT + "): " + e.getMessage());
         } finally {
 //            dataBase.disconnect();
-//            try {
-//                serverSocket.close();
-//            } catch (IOException e) {
-//                System.err.println("Ошибка при закрытии сервера(порт " + Constants.SERVER_PORT + ") :" + e.getMessage());
-//            }
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                System.err.println("Ошибка при закрытии сервера(порт " + Constants.SERVER_PORT + ") :" + e.getMessage());
+            }
         }
     }
 
@@ -43,10 +56,10 @@ public class ServerCore {
                         System.out.println(Constants.USER_LIST + " - подключенные пользователи");
                         break;
                     case Constants.RESTART:
-//                        serverSocket.close();
-//                        System.out.println("Перезапуск сервера(порт " + Constants.SERVER_PORT + ")");
-//                        new Thread(startServer()).start();
-//                        break;
+                        serverSocket.close();
+                        System.out.println("Перезапуск сервера(порт " + Constants.SERVER_PORT + ")");
+                        new Thread(startServer()).start();
+                        break;
                     case Constants.EXIT:
                         System.out.println("Остановка работы сервера(порт " + Constants.SERVER_PORT + ")");
                         return;
@@ -66,18 +79,28 @@ public class ServerCore {
     // запуск сервера(нить, ответственная за обработку подключений клиентов)
     private Runnable startServer() {
         return () -> {
-//            connectedUsers.clear();
-//            usersExecutorService = Executors.newCachedThreadPool();
-//            try {
-//            serverSocket = new ServerSocket(Constants.SERVER_PORT);
-//                System.out.println("Сервер(порт " + Constants.SERVER_PORT + ") запущен...Ожидание подключения клиентов...");
-//                usersProcessing();
-//            } catch (SocketException e) {
-//                System.err.println("Сервер(порт " + Constants.SERVER_PORT + ") прекратил обработку" +
-//                        " клиентов по причине: " + e.getMessage());
-//            } catch (IOException e) {
-//                System.err.println("При запуске сервера(порт " + Constants.SERVER_PORT + ") произошла ошибка: " + e.getMessage());
-//            }
+            connectedUsers.clear();
+            usersExecutorService = Executors.newCachedThreadPool();
+            try {
+            serverSocket = new ServerSocket(Constants.SERVER_PORT);
+                System.out.println("Сервер(порт " + Constants.SERVER_PORT + ") запущен...Ожидание подключения клиентов...");
+                usersProcessing();
+            } catch (SocketException e) {
+                System.err.println("Сервер(порт " + Constants.SERVER_PORT + ") прекратил обработку" +
+                        " клиентов по причине: " + e.getMessage());
+            } catch (IOException e) {
+                System.err.println("При запуске сервера(порт " + Constants.SERVER_PORT + ") произошла ошибка: " + e.getMessage());
+            }
         };
+    }
+
+    // работа с клиентами
+    private void usersProcessing() throws IOException{
+        // получение данных от подключившихся пользователей и создание для них отдельных нитей в пуле
+        while (!serverSocket.isClosed()) {
+            Socket socket = serverSocket.accept();
+            System.out.println("Клиент присоединился: IP: " + socket.getInetAddress() + " Порт: " + socket.getLocalPort());
+            usersExecutorService.submit(new UserHandler(this, socket));
+        }
     }
 }
