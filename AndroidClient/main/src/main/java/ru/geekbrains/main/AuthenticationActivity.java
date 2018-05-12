178202536d19d5161caf27f43e261b28bbfaad72
dimasan92 <dimasan92@gmail.com>
2018-05-12 14:12:44 +0300

    Module "main"

---
+++ AndroidClient/main/src/main/java/ru/geekbrains/main/AuthenticationActivity.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
